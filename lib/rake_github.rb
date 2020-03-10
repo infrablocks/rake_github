8ab8a82bc624c454d07585bb1fc5fece2fd75b1f
Toby Clemson <tobyclemson@gmail.com>
2020-03-10 09:46:40 +0000

    Add factory function for deploy key task set.

--- lib/rake_github.rb
+++ lib/rake_github.rb
@@ -3,4 +3,7 @@ require 'rake_github/tasks'
 require 'rake_github/task_sets'
 
 module RakeGithub
+  def self.define_deploy_key_tasks(opts = {}, &block)
+    RakeGithub::TaskSets::DeployKey.define(opts, &block)
+  end
 end
--- spec/rake_github_spec.rb
+++ spec/rake_github_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe RakeGithub do
   it 'has a version number' do
     expect(RakeGithub::VERSION).not_to be nil
   end
+
+  context 'define_deploy_key_tasks' do
+    context 'when instantiating RakeGithub::TaskSets::DeployKey' do
+      it 'passes the provided block' do
+        opts = {
+            repository: 'org/repo',
+            title: 'some-deploy-key'
+        }
+
+        block = lambda do |t|
+          t.access_token = 'some-token'
+          t.public_key = File.read('spec/fixtures/ssh.public')
+        end
+
+        expect(RakeGithub::TaskSets::DeployKey)
+            .to(receive(:define) do |passed_opts, &passed_block|
+              expect(passed_opts).to(eq(opts))
+              expect(passed_block).to(eq(block))
+            end)
+
+        RakeGithub.define_deploy_key_tasks(opts, &block)
+      end
+    end
+  end
 end
